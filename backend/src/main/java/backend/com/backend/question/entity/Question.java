754ce14306797748eec7ec1c2fb200d2ae3cd81c
xyz-wr <pponi13468@naver.com>
2023-04-17 16:02:05 +0900

    test

---
+++ backend/src/main/java/backend/com/backend/question/entity/Question.java
@@ -0,0 +1,26 @@
+package backend.com.backend.question.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long questionId;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String body;
+
+
+}
