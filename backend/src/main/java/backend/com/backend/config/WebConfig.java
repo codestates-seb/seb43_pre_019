ae3009340f4b2dc73ab61e7f372e354c4f031b71
NOTE111 <lolly5488@gmail.com>
2023-04-28 00:18:54 +0900

    [BE] Fix : 모든 오리진 허용

--- backend/src/main/java/backend/com/backend/config/WebConfig.java
+++ backend/src/main/java/backend/com/backend/config/WebConfig.java
@@ -6,13 +6,11 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Value("${config.domain}")
-    private String domain;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(domain)
+                .allowedOrigins("*")
                 .allowedMethods("POST", "GET", "PATCH", "DELETE", "OPTIONS");
     }
 }
